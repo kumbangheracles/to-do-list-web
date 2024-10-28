3639710d805d5d00c8532b7d438ec3e41c461d5c
kumbangheracles <kumbangheracles@gmail.com>
2024-10-28 14:35:25 +0700

    first commit

---
+++ scripts.js
@@ -0,0 +1,45 @@
+const addButton = document.getElementById('addButton')
+addButton.addEventListener('click', function(){
+    const task = document.getElementById('task').value;
+    let contentList = document.querySelector('.content-exist')
+    let newElement = document.createElement('p');
+    // let newText = document.createTextNode(task);
+    newElement.textContent = task;
+    let bungkusContent = document.createElement('div');
+    // tombol close list
+    let close = document.createElement('a');
+    close.textContent = 'x';
+    close.style.cursor = 'pointer';
+        close.addEventListener('click', function(e){
+            e.target.parentElement.remove();
+            e.preventDefault();
+        })
+    
+    //styling div tiap list
+    bungkusContent.style.display = 'flex'
+    bungkusContent.style.justifyContent = 'space-around'
+
+
+    // menambahkan checkbox setelah elemen
+    let checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+
+    bungkusContent.appendChild(newElement);
+    bungkusContent.appendChild(checkbox);
+    bungkusContent.appendChild(close);
+    //menambahkan bungkusContent kedalam contentList
+    contentList.appendChild(bungkusContent);
+
+    if(task === ''){
+        alert('Task Harus Diisi!!!')
+        bungkusContent.innerHTML = '';
+    }
+});
+
+
+//clear button
+const clear = document.querySelector('.clear');
+clear.addEventListener('click', function(){
+    let contentList = document.querySelector('.content-exist');
+    contentList.innerHTML = '';
+})
